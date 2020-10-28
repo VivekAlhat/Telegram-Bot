df5f6ebac5e8e6480463fa990e4a88fb06fa1934
VivekAlhat <vivekalhat14@outlook.com>
2020-10-28 11:09:43 +0530

    Create /bored Command

--- bot.js
+++ bot.js
@@ -1,16 +1,34 @@
 require("dotenv").config();
+const _ = require("lodash");
 const { default: Telegraf } = require("telegraf");
 const commands = require("./utils/commands");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 /* start command */
-bot.start((ctx) => ctx.reply("Hello! My name is Acaztron. Nice to meet you!"));
+bot.start((ctx) =>
+  ctx.reply(
+    "Hello! I am Acaztron the bot. Nice to meet you!\nUse /help command to know things that I can do for you"
+  )
+);
 
 /* help command */
-bot.help((ctx) => ctx.reply("Currently I don't have much to do"));
+bot.help((ctx) =>
+  ctx.reply(
+    "/start - Initialize Bot\n/help - Get Help\n/code - Github Repository Of Bot\n/bored - Get Random Activity To Do"
+  )
+);
 
 /* ---- Custom Commands ---- */
+
+/* get code repo */
+bot.command("code", (ctx) => ctx.reply(commands.getCodeRepository()));
+
+/* get random activity */
+bot.command("bored", (ctx) => {
+  const data = commands.getRandomActivity();
+  data.then((e) => ctx.reply(`You can ${_.lowerFirst(e.activity)}`));
+});
 
 /* launch bot */
 bot.launch();
--- utils/commands.js
+++ utils/commands.js
@@ -2,6 +2,16 @@
 
 const { default: Axios } = require("axios");
 
-class Features {}
+class Features {
+  getCodeRepository = () => {
+    return "https://github.com/VivekAlhat/Telegram-Bot";
+  };
+
+  getRandomActivity = async () => {
+    const response = await Axios.get("http://www.boredapi.com/api/activity/");
+    const data = await response.data;
+    return data;
+  };
+}
 
 module.exports = new Features();
